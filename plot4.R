2432222861da56c2ca1ea14fca972ab66a1a9030
siminxu <xusimin924@gmail.com>
2017-06-05 19:08:16 +0800

    add plot4

---
+++ plot4.R
@@ -0,0 +1,59 @@
+
+#read the table
+file="household_power_consumption.txt"
+
+
+power <- read.table(file, header=TRUE, sep=";", na.strings = "?", 
+                    colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+
+
+#as.date
+power$Date <- as.Date(power$Date, "%d/%m/%Y")
+
+
+## select Feb. 1, 2007 to Feb. 2, 2007
+power <- subset(power,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+
+## Remove incomplete observation
+power <- power[complete.cases(power),]
+
+
+## Combine Date and Time 
+dateTime <- paste(power$Date, power$Time)
+
+
+## Name the dateTime
+dateTime <- setNames(dateTime, "DateTime")
+
+## select not date and time
+library(dplyr)
+power <- select(power, -Date)
+power <- select(power, -Time)
+
+##combine DateTime and power
+power <- cbind(dateTime, power)
+
+## Format dateTime Column
+power$dateTime <- as.POSIXct(dateTime)
+
+
+##plot4
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+with(power, {
+        plot(Global_active_power~dateTime, type="l", 
+             ylab="Global Active Power (kilowatts)", xlab="")
+        plot(Voltage~dateTime, type="l", 
+             ylab="Voltage (volt)", xlab="")
+        plot(Sub_metering_1~dateTime, type="l", 
+             ylab="Global Active Power (kilowatts)", xlab="")
+        lines(Sub_metering_2~dateTime,col='Red')
+        lines(Sub_metering_3~dateTime,col='Blue')
+        legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+               legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+        plot(Global_reactive_power~dateTime, type="l", 
+             ylab="Global Rective Power (kilowatts)",xlab="")
+})
+
+## Save file and close device
+dev.copy(png,"plot4.png")
+dev.off()
